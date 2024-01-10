43979a4d24b236182a1c73718f6d2e621b3a4a2f
Jan Dvořák <dvorakjan@users.noreply.github.com>
2024-01-10 12:38:18 +0100

    fix: use browser version of ArrayBuffer when available

--- src/BotChatWithPolyfill.ts
+++ src/BotChatWithPolyfill.ts
@@ -1,11 +1,19 @@
 export * from './BotChat';
 
+// backup browser ArrayBuffer in case it gets overwritten by a polyfill - see below
+const browserArrayBuffer = global.ArrayBuffer
+
 // below are shims for compatibility with old browsers (IE 10 being the main culprit)
 import 'core-js'
 import 'core-js/modules/es6.string.starts-with';
 import 'core-js/modules/es6.array.find';
 import 'core-js/modules/es6.array.find-index';
 import 'isomorphic-fetch';
+
+// fix https://github.com/zloirock/core-js/issues/273 by using browser ArrayBuffer when available - https://feedyou.bitrix24.eu/company/personal/user/1/tasks/task/view/31031/
+if (typeof browserArrayBuffer !== 'undefined') {
+  global.ArrayBuffer = browserArrayBuffer
+}
 
 // Polyfill Promise if needed
 if (typeof (window as any).Promise === 'undefined') {
