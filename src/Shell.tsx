5b9781bfae63349a3ab1f9f725da66e3654540d7
jan.badura <jan.badura@geekcoders.cz>
2024-08-19 14:46:24 +0200

    fix: dropzone

--- src/Shell.tsx
+++ src/Shell.tsx
@@ -268,17 +268,134 @@ class ShellContainer extends React.Component<Props, State> implements ShellFunct
 
         const buttonAttachment = 
                 [
-                    <label className="wc-upload" htmlFor="wc-upload-input" tabIndex={0}><svg><path d="M19.96 4.79m-2 0a2 2 0 0 1 4 0 2 2 0 0 1-4 0zM8.32 4.19L2.5 15.53 22.45 15.53 17.46 8.56 14.42 11.18 8.32 4.19ZM1.04 1L1.04 17 24.96 17 24.96 1 1.04 1ZM1.03 0L24.96 0C25.54 0 26 0.45 26 0.99L26 17.01C26 17.55 25.53 18 24.96 18L1.03 18C0.46 18 0 17.55 0 17.01L0 0.99C0 0.45 0.47 0 1.03 0Z"></path></svg></label>,
+                    <label onKeyPress={evt => this.handleUploadButtonKeyPress(evt)}  className="wc-upload" htmlFor="wc-upload-input" tabIndex={0}><svg><path d="M19.96 4.79m-2 0a2 2 0 0 1 4 0 2 2 0 0 1-4 0zM8.32 4.19L2.5 15.53 22.45 15.53 17.46 8.56 14.42 11.18 8.32 4.19ZM1.04 1L1.04 17 24.96 17 24.96 1 1.04 1ZM1.03 0L24.96 0C25.54 0 26 0.45 26 0.99L26 17.01C26 17.55 25.53 18 24.96 18L1.03 18C0.46 18 0 17.55 0 17.01L0 0.99C0 0.45 0.47 0 1.03 0Z"></path></svg></label>,
                     <input
                         id="wc-upload-input"
                         tabIndex={-1}
-
+                        type="file"
+                        ref={input => this.fileInput = input}
+                        // multiple
                         onChange={() => this.onChangeFile()}
                         aria-label={this.props.strings.uploadFile}
                         role="button"
+                        capture={!!this.props.uploadCapture}
+                        accept={this.props.uploadCapture}
                     />,
                     <button className="wc-upload-screenshot" onClick={() => { this.takeScreenshot() }}><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="camera" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#8a8a8a" d="M512 144v288c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V144c0-26.5 21.5-48 48-48h88l12.3-32.9c7-18.7 24.9-31.1 44.9-31.1h125.5c20 0 37.9 12.4 44.9 31.1L376 96h88c26.5 0 48 21.5 48 48zM376 288c0-66.2-53.8-120-120-120s-120 53.8-120 120 53.8 120 120 120 120-53.8 120-120zm-32 0c0 48.5-39.5 88-88 88s-88-39.5-88-88 39.5-88 88-88 88 39.5 88 88z"></path></svg></button>
                 ]
+
+           const autosuggestInput = <Downshift
+                onChange={async (selection) => {
+                  this.sendMessage(selection)
+                  return 
+                }}
+                onInputValueChange={(inputValue) => {
+                    if(this.props.autoSuggestType === "static") {
+                        return
+                    }
+                    this.props.onChangeText(inputValue)
+                    return
+                }}
+                itemToString={(item) => {
+                  return item ? item : "";
+                }}
+              >
+                {({
+                  getInputProps,
+                  getItemProps,
+                  getMenuProps,
+                  isOpen,
+                  inputValue,
+                  highlightedIndex,
+                  selectedItem,
+                  getRootProps,
+                }) => (
+                  <div
+                    className="wc-textbox"
+                    {...getRootProps({ refKey: "" }, { suppressRefError: true })}
+                  >
+                    <input
+                      type="text"
+                      className="wc-shellinput"
+                      ref={(input) => (this.textInput = input)}
+                      autoFocus
+                      value={this.props.inputText}
+                      onBlur={async () => {
+                        if(this.props.autoSuggestType === "static") {
+                          return this.props.onChangeText("");
+                        }
+                      }}
+                      onKeyPress={(e) => {
+                        this.onKeyPress(e);
+                      }}
+                      onKeyUp={this.autoSuggestOnKeyUp}
+                      onFocus={() => this.onTextInputFocus()}
+                      placeholder={placeholder}
+                      disabled={this.props.disableInput}
+                      aria-label={this.props.inputText ? null : placeholder}
+                      aria-live="polite"
+                      {...getInputProps()}
+                    />
+                    <ul
+                      style={{
+                        position: "absolute",
+                        bottom: "100%",
+                        left: 13,
+                        minWidth: 200,
+                        borderTopLeftRadius: "13px",
+                        borderTopRightRadius: "13px",
+                        padding: 0,
+                        overflow: "hidden",
+                        backgroundColor: "#eceff1",
+                      }}
+                      {...getMenuProps()}
+                    >
+                      {isOpen
+                        ? Array.from(fuzzysort.go(inputValue, (this.props.autoSuggestItems.length > 0
+                            ? this.props.autoSuggestItems 
+                            : this.state.items), {keys: ["answer"], limit: 10}))
+                          .reverse()
+                          .map((item: any, index: number) => {
+                              return <li
+                                {...getItemProps({
+                                  key: item.obj.answer,
+                                  index,
+                                  item: item.obj.answer,
+                                  style: {
+                                    backgroundColor:
+                                      highlightedIndex === index
+                                        ? "lightgray"
+                                        : "transparent",
+                                    fontWeight:
+                                      selectedItem === item ? "bold" : "normal",
+                                    padding: "10px",
+                                  },
+                                })}
+                              >
+                                {item.obj.answer}
+                              </li>
+                            })
+                        : null}
+                    </ul>
+                  </div>
+                )}
+              </Downshift>
+
+                const defaultInput = <div className="wc-textbox">
+                <textarea
+                    className="wc-shellinput"
+                    ref={input => this.textInput = input}
+                    autoFocus
+                    value={this.props.inputText}
+                    onChange={_ => this.props.onChangeText(this.textInput.value)}
+                    onKeyPress={e => this.onKeyPress(e)}
+                    onFocus={() => this.onTextInputFocus()}
+                    placeholder={placeholder}
+                    disabled={this.props.disableInput}
+                    aria-label={this.props.inputText ? null : placeholder}
+                    aria-live="polite"
+                ></textarea>
+            </div>
 
         return (
             <div className={className}>
@@ -286,119 +403,7 @@ class ShellContainer extends React.Component<Props, State> implements ShellFunct
                     this.props.showUploadButton && (this.props.uploadUsingQrCodeOnly ? qrOnlyAttachment : this.props.uploadUsingDndAndQrCode ? localAndQrAttachment : buttonAttachment)
               }
 
-        {this.props.showAutoSuggest ? (
-          <Downshift
-            onChange={async (selection) => {
-              this.sendMessage(selection)
-              return 
-            }}
-            onInputValueChange={(inputValue) => {
-                if(this.props.autoSuggestType === "static") {
-                    return
-                }
-                this.props.onChangeText(inputValue)
-                return
-            }}
-            itemToString={(item) => {
-              return item ? item : "";
-            }}
-          >
-            {({
-              getInputProps,
-              getItemProps,
-              getMenuProps,
-              isOpen,
-              inputValue,
-              highlightedIndex,
-              selectedItem,
-              getRootProps,
-            }) => (
-              <div
-                className="wc-textbox"
-                {...getRootProps({ refKey: "" }, { suppressRefError: true })}
-              >
-                <input
-                  type="text"
-                  className="wc-shellinput"
-                  ref={(input) => (this.textInput = input)}
-                  autoFocus
-                  value={this.props.inputText}
-                  onBlur={async () => {
-                    if(this.props.autoSuggestType === "static") {
-                      return this.props.onChangeText("");
-                    }
-                  }}
-                  onKeyPress={(e) => {
-                    this.onKeyPress(e);
-                  }}
-                  onKeyUp={this.autoSuggestOnKeyUp}
-                  onFocus={() => this.onTextInputFocus()}
-                  placeholder={placeholder}
-                  disabled={this.props.disableInput}
-                  aria-label={this.props.inputText ? null : placeholder}
-                  aria-live="polite"
-                  {...getInputProps()}
-                />
-                <ul
-                  style={{
-                    position: "absolute",
-                    bottom: "100%",
-                    left: 13,
-                    minWidth: 200,
-                    borderTopLeftRadius: "13px",
-                    borderTopRightRadius: "13px",
-                    padding: 0,
-                    overflow: "hidden",
-                    backgroundColor: "#eceff1",
-                  }}
-                  {...getMenuProps()}
-                >
-                  {isOpen
-                    ? Array.from(fuzzysort.go(inputValue, (this.props.autoSuggestItems.length > 0
-                        ? this.props.autoSuggestItems 
-                        : this.state.items), {keys: ["answer"], limit: 10}))
-                      .reverse()
-                      .map((item: any, index: number) => {
-                          return <li
-                            {...getItemProps({
-                              key: item.obj.answer,
-                              index,
-                              item: item.obj.answer,
-                              style: {
-                                backgroundColor:
-                                  highlightedIndex === index
-                                    ? "lightgray"
-                                    : "transparent",
-                                fontWeight:
-                                  selectedItem === item ? "bold" : "normal",
-                                padding: "10px",
-                              },
-                            })}
-                          >
-                            {item.obj.answer}
-                          </li>
-                        })
-                    : null}
-                </ul>
-              </div>
-            )}
-          </Downshift>
-
-        ) : ( <div className="wc-textbox">
-        <textarea
-            className="wc-shellinput"
-            ref={input => this.textInput = input}
-            autoFocus
-            value={this.props.inputText}
-            onChange={_ => this.props.onChangeText(this.textInput.value)}
-            onKeyPress={e => this.onKeyPress(e)}
-            onFocus={() => this.onTextInputFocus()}
-            placeholder={placeholder}
-            disabled={this.props.disableInput}
-            aria-label={this.props.inputText ? null : placeholder}
-            aria-live="polite"
-        ></textarea>
-    </div> )}
+              {this.props.showAutoSuggest ? autosuggestInput : defaultInput}
                 
                 <button
                     className={sendButtonClassName}
