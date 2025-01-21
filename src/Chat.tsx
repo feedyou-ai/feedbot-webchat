4c47f6c1ba7b7e26764ee9b73db1fbd959a7e5de
Jan Dvořák <dvorakjan@users.noreply.github.com>
2025-01-21 23:48:03 +0100

    feat: skip fallback non-streaming message

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -179,6 +179,10 @@ export class Chat extends React.Component<ChatProps, {}> {
                 }
                 break;
             case "message":
+                if (activity.channelData && activity.channelData.alreadyStreamed) {
+                    break
+                }
+
                 this.store.dispatch<ChatActions>({ type: activity.from.id === state.connection.user.id ? 'Receive_Sent_Message' : 'Receive_Message', activity });
                 break;
 
